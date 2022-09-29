1f61f9becb07bc2cc4d2acdbd6d9dc55ed861409
atif memon <memonatif690@gmail.com>
2022-09-29 18:09:15 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Temp from "./component";
+
+const App = () => {
+  <div>
+    <Temp />
+  </div>;
+};
+
+export default App;
---
+++ src/component/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import "./App.css";
+
+const Temp = () => {
+  <div>
+    {/* <div className="main-temp">
+      <div className="tempreture">
+        <div className="disply">
+          <p> 87°C °F Precipitation: 1% Humidity: 56% Wind: 13 mph</p>
+        </div>
+        hchc
+      </div>
+    </div> */}
+    <h1>sarib</h1>
+  </div>;
+};
+
+export default Temp;
